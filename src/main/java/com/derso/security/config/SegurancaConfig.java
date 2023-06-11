1dccb4e9e1e454295c1cec9cb232c64ed78eed44
Éderson Cássio <edersoncassiolf@outlook.com>
2023-06-10 23:30:32 -0300

    Imports não usados mais, veja doc linkada!

--- src/main/java/com/derso/security/config/SegurancaConfig.java
+++ src/main/java/com/derso/security/config/SegurancaConfig.java
@@ -7,8 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
