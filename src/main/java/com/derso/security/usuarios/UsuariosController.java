bedf77ed123e55720c735b0e06ed56b7244e3611
Éderson Cássio <edersoncassiolf@outlook.com>
2023-06-11 02:15:05 -0300

    PUTA QUE PARIU

--- src/main/java/com/derso/security/usuarios/UsuariosController.java
+++ src/main/java/com/derso/security/usuarios/UsuariosController.java
@@ -3,12 +3,14 @@ package com.derso.security.usuarios;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 
-@RestController("/usuarios")
+@RestController
+@RequestMapping("/usuarios")
 public class UsuariosController {
 	
 	@Autowired
